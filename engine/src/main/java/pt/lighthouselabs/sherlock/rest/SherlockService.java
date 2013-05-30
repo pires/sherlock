e3c6ed30e142a20528b1b2d847c0cf1323c9c1e4
Paulo Pires <pjpires@gmail.com>
2013-05-30 12:53:16 +0100

    Implemented Sherlock REST endpoint and related tests.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

---
+++ engine/src/main/java/pt/lighthouselabs/sherlock/rest/SherlockService.java
@@ -0,0 +1,85 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package pt.lighthouselabs.sherlock.rest;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import pt.lighthouselabs.sherlock.dao.AuditRecordDao;
+import pt.lighthouselabs.sherlock.filters.audit.Audit;
+import pt.lighthouselabs.sherlock.model.AuditRecord;
+
+/**
+ * Sherlock REST Endpoint.
+ */
+@Stateless
+@Path("/")
+public class SherlockService {
+
+	@EJB
+	private AuditRecordDao arDao;
+
+	/**
+	 * Triggers and audit-event.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/fire")
+	@Audit("fire-event")
+	public Response fire() {
+		return Response.ok().build();
+	}
+
+	/**
+	 * @return a list of all persisted {@link AuditRecord}.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/list")
+	public List<AuditRecord> retrieveAllAuditRecords() {
+		return arDao.findAll();
+	}
+
+	/**
+	 * @return a list of all persisted {@link AuditRecord} by application ID.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/list/by_app_id")
+	public List<AuditRecord> retrieveAllAuditRecordsFromAppId(
+	        @QueryParam("appId") String appId) {
+		return arDao.find_all_by_appId(appId);
+	}
+
+	/**
+	 * @return a list of all persisted {@link AuditRecord} by application ID and
+	 *         by time-window.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/list/by_app_id_and_between_timestamps")
+	public List<AuditRecord> retrieveAllAuditRecordsFromAppIdAndBetweenTimestamps(
+	        @QueryParam("appId") String appId, @QueryParam("begin") Long begin,
+	        @QueryParam("end") Long end) {
+		return arDao.find_all_by_appId_and_between_time_interval(appId, begin,
+		        end);
+	}
+}
---
+++ engine/src/test/java/pt/lighthouselabs/sherlock/service/SherlockServiceTest.java
@@ -0,0 +1,136 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package pt.lighthouselabs.sherlock.service;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.joda.time.DateTime;
+import org.testng.annotations.Test;
+
+import pt.lighthouselabs.sherlock.RESTClient;
+import pt.lighthouselabs.sherlock.SetupTestSuite;
+import pt.lighthouselabs.sherlock.model.AuditRecord;
+import pt.lighthouselabs.sherlock.rest.SherlockService;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Tests {@link SherlockService}.
+ */
+public class SherlockServiceTest {
+
+	private static final String URL = "http://localhost:"
+	        + SetupTestSuite.SERVER_PORT + "/sherlock";
+
+	/**
+	 * Tests firing an audit event.
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	@Test
+	public void test_fire_audit_event() throws ClientProtocolException,
+	        IOException {
+		HttpResponse response = RESTClient.doHttpGet(URL.concat("/fire"), null,
+		        null);
+		assertEquals(response.getStatusLine().getStatusCode(), 200);
+	}
+
+	/**
+	 * Tests reading a list of {@link AuditRecord}.
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	@Test
+	public void test_list_all_auditrecords() throws ClientProtocolException,
+	        IOException {
+		HttpResponse response = RESTClient.doHttpGet(URL.concat("/list"), null,
+		        null);
+		assertEquals(response.getStatusLine().getStatusCode(), 200);
+		Type paginateType = new TypeToken<List<AuditRecord>>() {
+		}.getType();
+		Reader reader = new InputStreamReader(
+		        response.getEntity().getContent(), "UTF-8");
+		List<AuditRecord> records = new Gson().fromJson(reader, paginateType);
+		assertTrue(records.size() > 0);
+	}
+
+	/**
+	 * Tests reading a list of {@link AuditRecord} by application ID.
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	@Test
+	public void test_list_all_auditrecords_by_appId()
+	        throws ClientProtocolException, IOException {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("appId", "APP2");
+
+		HttpResponse response = RESTClient.doHttpGet(
+		        URL.concat("/list/by_app_id"), null, params);
+		assertEquals(response.getStatusLine().getStatusCode(), 200);
+		Type paginateType = new TypeToken<List<AuditRecord>>() {
+		}.getType();
+		Reader reader = new InputStreamReader(
+		        response.getEntity().getContent(), "UTF-8");
+		List<AuditRecord> records = new Gson().fromJson(reader, paginateType);
+		for (AuditRecord record : records)
+			assertEquals(record.getAppIdIndex(), "APP2");
+	}
+
+	/**
+	 * Tests reading a list of {@link AuditRecord} by application ID and between
+	 * time window.
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	@Test
+	public void test_list_all_auditrecords_by_appId_and_between_timestamp()
+	        throws ClientProtocolException, IOException {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("appId", "APP1");
+		params.put("begin", "1");
+		final Long timestamp = DateTime.now().getMillis();
+		params.put("end", Long.toString(timestamp));
+		HttpResponse response = RESTClient.doHttpGet(
+		        URL.concat("/list/by_app_id_and_between_timestamps"), null,
+		        params);
+		assertEquals(response.getStatusLine().getStatusCode(), 200);
+		Type paginateType = new TypeToken<List<AuditRecord>>() {
+		}.getType();
+		Reader reader = new InputStreamReader(
+		        response.getEntity().getContent(), "UTF-8");
+		List<AuditRecord> records = new Gson().fromJson(reader, paginateType);
+		for (AuditRecord record : records) {
+			assertEquals(record.getAppIdIndex(), "APP1");
+			assertTrue(record.getId().getTimestamp() > 1L);
+			assertTrue(record.getId().getTimestamp() < timestamp);
+		}
+	}
+
+}
